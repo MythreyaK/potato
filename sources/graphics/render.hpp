760721f2b07ba428578305e5fc7de756a504505f
Mythreya K <mythreya.github@gmail.com>
2021-05-25 02:03:56 +0530

    Use new context class in renderer

--- sources/graphics/render.hpp
+++ sources/graphics/render.hpp
@@ -11,11 +11,13 @@ namespace potato::render {
 
     class render_instance {
         context render_context;
+        device  render_device;
 
       public:
         render_instance(const std::vector<std::string> required_extensions,
                         GLFWwindow*                    window_handle)
-          : render_context(window_handle) {}
+          : render_context(window_handle)
+          , render_device(render_context.get_instance(), window_handle) {}
     };
 
 }  // namespace potato::render
