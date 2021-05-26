81cb676f60c6a5c51bccd82b703b5e82cdfe4e7b
Mythreya K <mythreya.github@gmail.com>
2021-05-26 10:52:40 +0530

    Fix for windows.h min-max macro

--- sources/graphics/vkinclude/vulkan.hpp
+++ sources/graphics/vkinclude/vulkan.hpp
@@ -3,6 +3,8 @@
 // This file ensures all vulkan.hpp includes in the
 // project use these preprosessor directives
 
+#define NOMINMAX  // windows.h hash-defines max macro, breaking stuff
+#define WIN32_LEAN_AND_MEAN
 #define VK_USE_PLATFORM_WIN32_KHR
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS  // enable designated initializers
