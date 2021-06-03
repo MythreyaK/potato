80e75fa958784032804e4814e3cd17cc4fe7bd75
Mythreya K <mythreya.github@gmail.com>
2021-06-03 21:11:24 +0530

    Add virtual functions for custom pipeline management

--- sources/graphics/render.cpp
+++ sources/graphics/render.cpp
@@ -8,23 +8,85 @@ namespace potato::render {
 
     render_instance::render_instance(GLFWwindow* window_handle)
       : window_handle(window_handle)
-      , context(window_handle)
-      , render_device(
-          std::make_shared<device>(context.get_instance(), window_handle))
-      , render_surface(window_handle, render_device->make_shared()){
+      , potato_context(window_handle)
+      , potato_device(std::make_shared<device>(potato_context.get_instance(),
+                                               window_handle))
+      , potato_surface(window_handle, potato_device->make_shared()) {
 
+        // ctor
         create_command_buffers(
-          render_device->info().queues.graphics_inx.value());
+          potato_device->info().queues.graphics_inx.value());
+    }
+
+    void render_instance::create_pipeline() {
+
+        const auto curr_extent { potato_surface.current_extent() };
+
+        // call virtual functions
+        renderpass =
+          create_renderpass(potato_device->logical(),
+                            potato_device->info().swapchain.surface_format);
+
+        auto pipeline_layout { create_pipeline_layout(
+          potato_device->logical()) };
+
+        auto pipeline_create_info { create_pipeline_info() };
+
+        // NOTE: Some items in default pipeline create info aren't set,
+        // so we set them here as needed.
+
+        // clang-format off
+        pipeline_create_info.info.scissor = vk::Rect2D {
+            .offset = {},
+            .extent = curr_extent
+        };
+
+        pipeline_create_info.info.viewport = vk::Viewport {
+            .x        = 0,
+            .y        = 0,
+            .width    = static_cast<float>(curr_extent.width),
+            .height   = static_cast<float>(curr_extent.height),
+            .minDepth = 0.0f,
+            .maxDepth = 1.0f
+        };
+        // clang-format on
+
+        potato_pipeline = pipeline(potato_device->make_shared(),
+                                   pipeline_create_info,
+                                   std::move(pipeline_layout),
+                                   renderpass);
+
+        potato_surface.create_framebuffers(renderpass);
     }
 
     void render_instance::window_resized() {
-        render_device->logical().waitIdle();
-        render_surface.reinitialize();
+        destroy_objects();
+        recreate_objects();
+    }
+
+    uint32_t render_instance::swapimage_count() const {
+        return potato_surface.swapimage_count();
     }
 
     render_instance::~render_instance() {
-        render_device->logical().destroyCommandPool(cmd_pool);
-        render_device->logical().freeCommandBuffers(cmd_pool, cmd_buffers);
+        destroy_objects();
+    }
+
+    void render_instance::recreate_objects() {
+        create_command_buffers(
+          potato_device->info().queues.graphics_inx.value());
+    }
+
+    void render_instance::destroy_objects() {
+        // for ( auto& fb : framebuffers ) {
+        //     potato_device->logical().destroyFramebuffer(fb);
+        // }
+
+        // potato_pipeline.~pipeline();
+        // potato_device->logical().destroyPipelineLayout(pipeline_layout);
+        // potato_device->logical().destroyRenderPass(renderpass);
+        potato_device->logical().freeCommandBuffers(cmd_pool, cmd_buffers);
+        potato_device->logical().destroyCommandPool(cmd_pool);
     }
 
 }  // namespace potato::render
--- sources/graphics/render.hpp
+++ sources/graphics/render.hpp
@@ -6,6 +6,7 @@
 // clang-format on
 #include "context.hpp"
 #include "device.hpp"
+#include "pipeline.hpp"
 #include "surface.hpp"
 
 #include <vector>
@@ -14,21 +15,29 @@ namespace potato::render {
 
     class render_instance {
       private:
-        using vkcmdbuffers = std::vector<vk::CommandBuffer>;
+        using vkcmdbuffers   = std::vector<vk::CommandBuffer>;
+        using vkframebuffers = std::vector<vk::Framebuffer>;
 
-        GLFWwindow*     window_handle;
-        context         context;
-        device          render_device;
-        surface         render_surface;
-        vk::CommandPool cmdpool {};
-        vkcmdbuffers    commandbuffers {};
+        GLFWwindow*             window_handle;
+        context                 potato_context;
+        std::shared_ptr<device> potato_device;
+        surface                 potato_surface;
+        vk::CommandPool         cmd_pool {};
+        vkcmdbuffers            cmd_buffers {};
+        vk::RenderPass          renderpass {};
+        pipeline                potato_pipeline {};
 
         void create_command_buffers(uint32_t graphics_queue);
-        void destroy_command_buffers();
+        void create_pipeline();
+        void destroy_objects();
+        void recreate_objects();
 
       public:
         render_instance(GLFWwindow* window_handle);
-        void window_resized();
+        ~render_instance();
+
+        void     window_resized();
+        uint32_t swapimage_count() const;
 
         // no copies
         render_instance(const render_instance&) = delete;
@@ -37,6 +46,14 @@ namespace potato::render {
         // allow move
         render_instance(render_instance&&) = default;
         render_instance& operator=(render_instance&&) = default;
+
+        // Virtual functions
+        virtual pipeline           create_framebuffers(const vk::Device&);
+        virtual vk::PipelineLayout create_pipeline_layout(const vk::Device&);
+        virtual pipeline_info      create_pipeline_info();
+
+        virtual vk::RenderPass create_renderpass(const vk::Device&,
+                                                 vk::Format color_format);
     };
 
 }  // namespace potato::render
